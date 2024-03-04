1444d35842771856c504a4f6ce360db6ded28c5f
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2024-03-04 11:43:45 +0530

    Add files via upload

--- findXinArray.cpp
+++ findXinArray.cpp
@@ -4,23 +4,25 @@ int main(){
     int n;
     cout<<"Enter the length of array:"<<endl; 
     cin>>n; 
-    int x;
-    cout<<"Enter the element for search:"<<endl; 
-    cin>>x; 
+    //int x;
+    //cout<<"Enter the element for search:"<<endl; 
+    //cin>>x; 
     int arr[n]; 
     for(int i=0;i<=n-1;i++){
         cout<<"Enter the elements in array:"<<endl; 
         cin>>arr[i]; 
         
     }
+    int x;
+    cout<<"Enter the element for search:"<<endl; 
+    cin>>x; 
+    bool flag=false; 
     for(int i=0;i<=n-1;i++){
         if(arr[i]==x){
-            cout<<"Present"<<endl;
+            flag=true; 
 
         }
-        else{
-            cout<<"Not present"<<endl; 
-        }
-        break; 
     }
+    if(flag==true) cout<<"Element found"; 
+    else cout<<"Element not found"; 
 }    
