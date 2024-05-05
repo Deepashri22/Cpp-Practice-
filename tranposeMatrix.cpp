43321d22cee360448d0471a46f6d7ceacaafbaee
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2024-05-05 09:58:49 +0530

    Add files via upload

---
+++ tranposeMatrix.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[2][4];
+    for(int i=0;i<=1;i++){
+        for(int j=0;j<=3;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<=1;i++){
+        for(int j=0;j<=3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl; 
+    }
+
+    //transpose
+    for(int j=0;j<4;j++){
+        for(int i=0;i<2;i++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl; 
+    }
+}
